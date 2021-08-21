0c68926f852e108cb7d8e63d4872341a3c097ef6
Dharshan Kumar <76487591+Dharsh03@users.noreply.github.com>
2021-08-21 13:31:58 +0530

    Add files via upload

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,39 @@
+export const navLinks = [
+  {
+    label: 'About',
+    pathname: '/projects/device-models',
+    hash: '#project-1',
+  },
+  {
+    label: 'Services',
+    pathname: '/projects/devtech-tools',
+  },
+  {
+    label: 'Hiring',
+    pathname: '/',
+    hash: '#about',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  
+  {
+    label: 'Email',
+    url: 'mailto:hb@positionskart.com',
+    icon: 'email',
+  },
+  {
+    label: 'Call',
+    url: 'tel:+914223573755',
+    icon: 'call',
+  },
+  {
+    label: 'Mobile',
+    url: 'tel:+919566615376',
+    icon: 'mobile',
+  }
+]
